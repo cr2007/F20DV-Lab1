d919cb03fb0fd812967c8e8b0c32e17fead23b2f
Chandrashekhar R <cr2007@hw.ac.uk>
2024-01-16 22:24:17 +0400

    Part2: Adds data for drawing charts
    Using citie's population and area data from Wikipedia

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,12 @@ let barSvg = barContainer.append('svg')
     .attr('width', 800)
     .attr('height', 500)
     .classed('barchart', true); // Add an svg child element to the div
+
+let cities = [
+    {city: 'Edinburgh', pop: 506000, area: 119, alt: 47},
+    {city: 'Dubai', pop: 3604000, area: 1610, alt: 5},
+    {city: 'Putrajaya', pop: 109000, area: 49, alt: 38},
+    {city: 'Qingdao', pop: 10071000, area: 11228, alt: 25},
+    {city: 'Lagos', pop: 8048000, area: 1171, alt: 41},
+    {city: 'Ottawa', pop: 1017000, area: 2790, alt: 70},
+]
