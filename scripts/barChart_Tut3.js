b68d9b1cbe15f5d8de1301620e85d0cd64ba2732
Chandrashekhar R <cr2007@hw.ac.uk>
2024-01-22 13:04:24 +0400

    Adds scaling and axes in the Bar Chart class
    * Adds methods for Updating Scales, Axes
    * Adds method for setting labels
    * Updates the attribute configuration in 'updateBars' method
    * Adds styling rules for 'text' in the CSS file

--- scripts/barChart_Tut3.js
+++ scripts/barChart_Tut3.js
@@ -11,7 +11,6 @@ export default class BarChart {
 	- margin: chart area margins [top, bottom, left, right]
 	*/
 	constructor(container, width, height, margin) {
-
 		this.width  = width;
 		this.height = height;
 		this.margin = margin;
@@ -21,9 +20,44 @@ export default class BarChart {
 			.attr("width", width).attr("height", height);
 
 		this.chart = this.svg.append("g")
-			.attr('transform', `translate(${this.margin[2]}, ${this.margin[0]})`);
+			.attr("transform",`translate(${this.margin[2]}, ${this.margin[0]})`);
 
 		this.bars = this.chart.selectAll("rect.bar");
+
+		// Axes
+		this.axisX = this.svg.append("g")
+			.attr("transform", `translate(0,${this.margin[2]}, $${this.height - this.margin[1]})`);
+		this.axisY = this.svg.append("g").attr("transform", `translate(${this.margin[2]}, ${this.margin[0]})`);
+
+		this.labelX = this.svg.append("text")
+			.attr("transform", `translate(${this.width / 2}, ${this.height})`)
+			.style("text-anchor", "middle").attr("dy", -5);
+
+		this.labelY = this.svg.append("text")
+			.attr("transform", `translate(0, ${this.margin[0]})rotate(-90)`)
+			.style("text-anchor", "end").attr("dy", 15);
+	}
+
+	#updateScales() {
+		let chartWidth = this.width - this.margin[2] - this.margin[3],
+			chartHeight = this.height - this.margin[0] - this.margin[1];
+
+		let rangeX = [0, chartWidth],
+			rangeY = [chartHeight, 0];
+
+		let domainX = this.data.map((d) => d[0]),
+			domainY = [0, d3.max(this.data, (d) => d[1])];
+
+		this.scaleX = d3.scaleBand(domainX, rangeX).padding(0.2);
+		this.scaleY = d3.scaleLinear(domainY, rangeY);
+	}
+
+	#updateAxes() {
+		let axisX = d3.axisBottom(this.scaleX),
+			axisY = d3.axisLeft(this.scaleY);
+
+		this.axisX.call(axisX);
+		this.axisY.call(axisY);
 	}
 
 	// Private methods
@@ -33,10 +67,10 @@ export default class BarChart {
 			.data(this.data, (d) => d[0])
 			.join("rect")
 			.classed("bar", true)
-			.attr("x", (d, i) => i * 40 + 5)
-			.attr("y", (d) => this.height - d[1] * 10)
-			.attr("width", 40)
-			.attr("height", (d) => d[1] * 10);
+			.attr("x", (d) => this.scaleX(d[0]))
+			.attr("y", (d) => this.scaleY(d[1]))
+			.attr("width", this.scaleX.bandwidth())
+			.attr("height", (d) => this.scaleY(0) - this.scaleY(d[1]));
 	}
 
 	// Public API
@@ -46,7 +80,15 @@ export default class BarChart {
 	// here we assume a [[k, v], ...] format for efficiency
 	render(dataset) {
 		this.data = dataset;
+		this.#updateScales();
 		this.#updateBars();
+		this.#updateAxes();
+		return this; // to allow chaining
+	}
+
+	setLabels(labelX = "categories", labelY = "values") {
+		this.labelX.text(labelX);
+		this.labelY.text(labelY);
 		return this; // to allow chaining
 	}
 }
